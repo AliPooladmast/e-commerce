697798ca0015bf0c61d8ea018e26908bf0d736c2
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 23:40:30 +0430

    Create and stylize arrow for sliders

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,18 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React from "react";
 import style from "./Slider.module.scss";
 
 const Slider = () => {
-  return <div className={style.Container}>Slider</div>;
+  return (
+    <div className={style.Container}>
+      <div className={style.Arrow}>
+        <ArrowLeftOutlined />
+      </div>
+      <div className={style.Arrow}>
+        <ArrowRightOutlined />
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
