5e84ad5477fccc35dcd3944ae43dcff939b6d713
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 19:32:27 +0430

    Feat: Add announcement section over the nav bar

---
+++ src/components/Announcement/Announcement.jsx
@@ -0,0 +1,9 @@
+import css from "./Announcement.module.scss";
+
+export const Announcement = () => {
+  return (
+    <div className={css.Container}>
+      Hurry Up! Invite Your Friend to Recieve Your First Gift From Us
+    </div>
+  );
+};
