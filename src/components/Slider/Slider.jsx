da3d4fc84349ee5e02a5509863cf30471dacdece
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 23:19:12 +0430

    Feat: Add slider component and its initial styles

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,7 @@
+import style from "./Slider.module.scss";
+
+const Slider = () => {
+  return <div className={style.Container}>Slider</div>;
+};
+
+export default Slider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Announcement from "../components/Announcement/Announcement";
 import NavBar from "../components/NavBar/NavBar";
+import Slider from "../components/Slider/Slider";
 
 const Home = () => {
   return (
     <>
       <Announcement />
       <NavBar />
+      <Slider />
     </>
   );
 };
