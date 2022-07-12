1b1f0ec4fccac84b76a612de8000ef05a1594fc0
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 19:25:55 +0430

    NavBar: Specify a seperated directory for nav bar files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import { Announcement } from "../components/Announcement/Announcement";
+import NavBar from "../components/NavBar/NavBar";
 
 const Home = () => {
-  return <NavBar />;
+  return (
+    <>
+      <Announcement />
+      <NavBar />
+    </>
+  );
 };
 
 export default Home;
