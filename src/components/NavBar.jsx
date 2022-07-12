31acfcd39b56e558433da44b3b2c13b6164a4597
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 12:53:17 +0430

    NavBar: Add wrapper with padding to navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import style from "./NavBar.module.scss";
 
 const NavBar = () => {
-  return <div className={style.Container}>NavBar</div>;
+  return (
+    <div className={style.Container}>
+      <div className={style.Wrapper}>NavBar</div>
+    </div>
+  );
 };
 
 export default NavBar;
