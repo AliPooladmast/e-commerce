548136837136fc1d949bcc0a36822c24a8eb2194
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 13:29:01 +0430

    Add language tag and style to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,9 @@ const NavBar = () => {
   return (
     <div className={style.Container}>
       <div className={style.Wrapper}>
-        <div className={style.Left}>left</div>
+        <div className={style.Left}>
+          <div className={style.Language}>EN</div>
+        </div>
         <div className={style.Center}>center</div>
         <div className={style.Right}>right</div>
       </div>
