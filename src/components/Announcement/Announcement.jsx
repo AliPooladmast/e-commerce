3f66508ab2dcadbe56d9c06027dab53e6f6819be
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 23:09:59 +0430

    Refactor: Change scss module name from css to style

--- src/components/Announcement/Announcement.jsx
+++ src/components/Announcement/Announcement.jsx
@@ -1,8 +1,8 @@
-import css from "./Announcement.module.scss";
+import style from "./Announcement.module.scss";
 
 const Announcement = () => {
   return (
-    <div className={css.Container}>
+    <div className={style.Container}>
       Hurry Up! Invite Your Friend to Recieve Your First Gift From Us
     </div>
   );
