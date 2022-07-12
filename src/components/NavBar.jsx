2ae00d5bc40b2ae2f51e25a54324e4bb97ead8a6
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-12 12:23:02 +0430

    NavBar: Add container to navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import style from "./NavBar.module.scss";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <div className={style.Container}>NavBar</div>;
 };
 
 export default NavBar;
