92e00b9c71a6e706a02b3275a928c024a5b2169c
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-16 18:56:03 +0430

    feat: pass background color as styles from data.js to slide item tags

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -12,7 +12,7 @@ const Slider = () => {
 
       <div className={style.Wrapper}>
         {sliderItems.map((item) => (
-          <div className={style["Slide--First"]}>
+          <div className={style.Slide} style={{ backgroundColor: item.bg }}>
             <div className={style.ImgContainer}>
               <img className={style.Image} src={item.img} alt="a fashion set" />
             </div>
@@ -23,32 +23,6 @@ const Slider = () => {
             </div>
           </div>
         ))}
-
-        {/* <div className={style["Slide--Second"]}>
-          <div className={style.ImgContainer}>
-            <img className={style.Image} src={fashionSet} alt="a fashion set" />
-          </div>
-          <div className={style.InfoContainer}>
-            <h1 className={style.Title}>WHOLE YEAR SALE</h1>
-            <p className={style.Desc}>
-              NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%
-            </p>
-            <button className={style.Button}>SHOP NOW</button>
-          </div>
-        </div>
-
-        <div className={style["Slide--Third"]}>
-          <div className={style.ImgContainer}>
-            <img className={style.Image} src={fashionSet} alt="a fashion set" />
-          </div>
-          <div className={style.InfoContainer}>
-            <h1 className={style.Title}>POPULAR SALE</h1>
-            <p className={style.Desc}>
-              NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%
-            </p>
-            <button className={style.Button}>SHOP NOW</button>
-          </div>
-        </div> */}
       </div>
 
       <div className={`${style.Arrow} ${style.Right}`}>
