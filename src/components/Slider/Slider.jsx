35933b654db1986afe7e08c6f5ed6d1b1c0a5c62
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-16 14:19:57 +0430

    Feat: Add fashion set image with its container and wrapper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import React from "react";
 import style from "./Slider.module.scss";
+import fashionSet from "../../assests/images/fashion-set.png";
 
 const Slider = () => {
   return (
@@ -8,6 +9,13 @@ const Slider = () => {
       <div className={`${style.Arrow} ${style.Left}`}>
         <ArrowLeftOutlined />
       </div>
+
+      <div className={style.Wrapper}>
+        <div className={style.ImgContainer}>
+          <img className={style.Image} src={fashionSet} alt="a fashion set" />
+        </div>
+      </div>
+
       <div className={`${style.Arrow} ${style.Right}`}>
         <ArrowRightOutlined />
       </div>
