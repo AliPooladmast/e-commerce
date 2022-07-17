92e4bb725780f81761c357de702659fb82ef8aad
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-17 20:07:55 +0430

    feat: set some style without importing cards

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { categories } from "../../data.js";
 import CategoryItem from "../CategoryItem/CategoryItem";
+import style from "./Categories.module.scss";
 
 function Categories() {
   return (
-    <div>
+    <div className={style.Container}>
       {categories.map((item) => (
         <CategoryItem />
       ))}
