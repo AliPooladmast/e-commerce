f5979f2632db9a9675833b9811c36ef34192e0eb
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-17 20:16:09 +0430

    feat: add image and info section to category items

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import style from "./CategoryItem.module.scss";
 
 const CategoryItem = () => {
-  return <div>categoryItem</div>;
+  return (
+    <div className={style.Container}>
+      <img className={style.Image} src="" alt="" />
+      <div className={style.Info}>
+        <h1></h1>
+        <button></button>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryItem;
