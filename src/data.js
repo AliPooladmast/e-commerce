044dcddc67c5e77692e87832bf6175c41b311f09
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-17 18:32:01 +0430

    feat: create a group of categories in data file

--- src/data.js
+++ src/data.js
@@ -2,6 +2,11 @@ import fashionSet from "./assests/images/fashion-set.jpg";
 import girlSunDress from "./assests/images/girl-sundress.jpg";
 import menCoat from "./assests/images/men-coat.jpg";
 import girlSunGlasses from "./assests/images/girl-sunglass.jpg";
+import casualDress from "./assests/images/casual-dress.jpg";
+import fashionMen from "./assests/images/fashion-men.jpg";
+import formalDress from "./assests/images/formal-dress.jpg";
+import menAccessories from "./assests/images/men-accessories.jpg";
+import weddingDress from "./assests/images/wedding-dress.jpg";
 
 export const sliderItems = [
   {
@@ -31,5 +36,23 @@ export const sliderItems = [
     title: "FASHION PORDUCTS",
     desc: "NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%",
     bg: "#f5fafd",
+  },
+];
+
+const categories = [
+  {
+    id: 1,
+    img: casualDress,
+    title: "Casual Outfit",
+  },
+  {
+    id: 2,
+    img: fashionMen,
+    title: "Fashion Style",
+  },
+  {
+    id: 3,
+    img: formalDress,
+    title: "Formal Dress",
   },
 ];
