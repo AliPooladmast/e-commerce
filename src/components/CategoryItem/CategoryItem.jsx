9f984cdee17541dd35428b72704ec85c5be808c2
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-17 20:34:29 +0430

    feat: set images style to align and resize them equally

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -5,7 +5,7 @@ const CategoryItem = ({ item }) => {
   console.log(item);
   return (
     <div className={style.Container}>
-      <img className={style.Image} src={item.img} alt={item.title} />
+      <img src={item.img} alt={item.title} />
       <div className={style.Info}>
         <h1>{item.title}</h1>
         <button>SHOP NOW</button>
