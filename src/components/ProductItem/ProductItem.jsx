2901dc2b0e193ade18ec89201efcfa5e94c80385
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-19 21:58:41 +0430

    ProductItem: Add circle, info and image section to the main module

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,7 +1,24 @@
+import {
+  ShoppingCartOutlined,
+  SearchOutlinedIcon,
+  FavoriteBorderOutlinedIcon,
+} from "@material-ui/icons";
 import React from "react";
+import style from "./ProductItem.module.scss";
 
-const ProductItem = () => {
-  return <div>ProductItem</div>;
+const ProductItem = ({ item }) => {
+  return (
+    <div className={style.Container}>
+      <div className={style.Circle}>
+        <img src="" alt="" />
+        <div className={style.Info}>
+          <div>
+            <ShoppingCartOutlined />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
