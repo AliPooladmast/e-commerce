ccfbdb1caca0c14cdd4d33053eaf4fd87b16fd14
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-19 21:24:15 +0430

    Products: Add style for arrangement of card items in product module

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { popularProducts } from "../../data";
 import ProductItem from "../ProductItem/ProductItem";
+import style from "./Products.module.scss";
 
 const Products = () => {
   return (
-    <div>
+    <div className={style.Container}>
       {popularProducts.map((item) => (
         <ProductItem item={item} key={item.id} />
       ))}
