ededa61128d3a46717d5d02ddf3cf092dc746afb
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-21 16:19:40 +0430

    feat(Footer): add html contents for footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
+import { Facebook, Instagram, Twitter, WhatsApp } from "@material-ui/icons";
 import React from "react";
+import style from "./Footer.module.scss";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={style.Container}>
+      <div className={style.Left}></div>
+      <div className={style.Center}></div>
+      <div className={style.Right}></div>
+    </div>
+  );
 }
 
 export default Footer;
