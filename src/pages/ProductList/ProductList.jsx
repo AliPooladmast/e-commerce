8be1be389350e9d0d27e12ac7c9e05976c7cff9c
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-22 22:17:56 +0430

    feat(productList): add anouncement and navigation bar to the product list page

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,8 +1,15 @@
+import Announcement from "../../components/Announcement/Announcement";
+import NavBar from "../../components/NavBar/NavBar";
 import React from "react";
 import style from "./ProductList.module.scss";
 
 function ProductList() {
-  return <div className={style.Container}></div>;
+  return (
+    <div className={style.Container}>
+      <Announcement />
+      <NavBar />
+    </div>
+  );
 }
 
 export default ProductList;
