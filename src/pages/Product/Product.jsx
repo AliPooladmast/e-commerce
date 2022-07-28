920fd5c723ed4e83cc489c7961005d4408bcb6ac
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-28 12:26:43 +0430

    feat(product): add contents for product pages like image and info sections

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -4,12 +4,28 @@ import NavBar from "../../components/NavBar/NavBar";
 import Announcement from "../../components/Announcement/Announcement";
 import Newsletter from "../../components/Newsletter/Newsletter";
 import Footer from "../../components/Footer/Footer";
+import manInCoat from "../../assests/images/coats.jpg";
 
 const Product = () => {
   return (
     <div className={style.Container}>
       <NavBar />
       <Announcement />
+      <div className={style.Wrapper}>
+        <div className={style.ImageContainer}>
+          <img src={manInCoat} alt="a man in coat" />
+        </div>
+        <div className={style.InfoContainer}>
+          <h1>Fur Coat</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+            explicabo iusto recusandae omnis, aut incidunt amet a expedita nobis
+            quo? Saepe sapiente ut doloremque asperiores, corrupti nulla
+            laboriosam fugiat aspernatur!
+          </p>
+          <span>$ 20</span>
+        </div>
+      </div>
       <Newsletter />
       <Footer />
     </div>
