c86ffe9815a1c4f433390bd2352133f2d7f5356a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-28 11:07:39 +0430

    feat(product-list): Add select-option element for color feature of the products

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -15,6 +15,17 @@ function ProductList() {
       <div className={style.FilterContainer}>
         <div>
           <span>Filter Products</span>
+          <select name="filter-product" id="filter-product-select">
+            <option value="color" disabled>
+              Color
+            </option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
+            <option value="red">Red</option>
+            <option value="yellow">Yellow</option>
+            <option value="blue">Blue</option>
+            <option value="green">Green</option>
+          </select>
         </div>
         <div>
           <span>Sort Product</span>
