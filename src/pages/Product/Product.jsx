7b5301694d6a6edf0eeac2ce084bacf1cf21bfd5
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-28 12:13:25 +0430

    feat(product): add common components for the itial framwork building of module

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import style from "./Product.module.scss";
+import NavBar from "../../components/NavBar/NavBar";
+import Announcement from "../../components/Announcement/Announcement";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/Footer/Footer";
 
 const Product = () => {
-  return <div className={style.Container}>Product</div>;
+  return (
+    <div className={style.Container}>
+      <NavBar />
+      <Announcement />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
 };
 
 export default Product;
