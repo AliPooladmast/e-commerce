6a47a95e8ab2fd7ac0492aa3b7d0bc64ee60900a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-07-28 12:07:40 +0430

    feat(product): create the product page for product item in the product list

---
+++ src/pages/Product/Product.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Product.module.scss";
+
+const Product = () => {
+  return <div className={style.Container}>Product</div>;
+};
+
+export default Product;
