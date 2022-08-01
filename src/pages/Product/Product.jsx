bc9e6e68e8e81b7acf0bdcce32ba587be4cbaf13
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-01 20:01:12 +0430

    feat(porduct): add contents for color and size filter of the product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -24,6 +24,24 @@ const Product = () => {
             laboriosam fugiat aspernatur!
           </p>
           <span>$ 20</span>
+          <div className={style.FilterContainer}>
+            <div className={style.Filter}>
+              <span>Color</span>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={style.Filter}>
+              <span>Size</span>
+              <select name="" id="">
+                <option value="">XS</option>
+                <option value="">S</option>
+                <option value="">M</option>
+                <option value="">L</option>
+                <option value="">XL</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
       <Newsletter />
