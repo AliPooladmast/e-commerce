79082bcb717d3085950d28b29155449ab7bfce0e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-08-04 10:03:51 +0430

    feat(register): create register page with its html contents for registering a form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
+import Register from "./pages/Register/Register";
 
 const App = () => {
-  return <Product />;
+  return <Register />;
 };
 
 export default App;
