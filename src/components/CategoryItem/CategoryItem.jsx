bf1c6fe1c37f4aa0ccdc8936a6315776bd03b954
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-15 23:23:35 +0430

    add link to category item to product category path

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import style from "./CategoryItem.module.scss";
 
 const CategoryItem = ({ item }) => {
   return (
     <div className={style.Container}>
-      <img src={item.img} alt={item.title} />
-      <div className={style.Info}>
-        <h1>{item.title}</h1>
-        <button>SHOP NOW</button>
-      </div>
+      <Link to={`products/${item.category}`}>
+        <img src={item.img} alt={item.title} />
+        <div className={style.Info}>
+          <h1>{item.title}</h1>
+          <button>SHOP NOW</button>
+        </div>
+      </Link>
     </div>
   );
 };
