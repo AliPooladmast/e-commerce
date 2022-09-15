2db4b2088b0c65a46a3c30f6f2b1d76911a7fc5d
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-15 22:19:28 +0430

    add navigate to home for users that are already logged in

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Login from "./pages/Login/Login";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
 import Register from "./pages/Register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  const user = true;
+
   return (
     <BrowserRouter>
       <Routes>
@@ -22,7 +24,10 @@ const App = () => {
 
         <Route path="/cart" element={<Cart />} />
 
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" replace /> : <Login />}
+        />
 
         <Route path="/register" element={<Register />} />
       </Routes>
