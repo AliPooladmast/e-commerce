a288a8ec91681577bca746cb0d99e53a93608950
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-16 17:00:01 +0430

    add axios to packages

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { popularProducts } from "../../data";
 import ProductItem from "../ProductItem/ProductItem";
 import style from "./Products.module.scss";
 
-const Products = () => {
+const Products = ({ category, filters, sort }) => {
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/products");
+        console.log(res);
+      } catch (err) {}
+    };
+    getProducts();
+  }, []);
+
   return (
     <div className={style.Container}>
       {popularProducts.map((item) => (
