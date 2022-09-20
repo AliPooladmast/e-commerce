79295e94ae5ea40958ac9ac921a46fdbbba3667e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-20 23:48:49 +0430

    add cart states and actions as cart redux

---
+++ src/redux/cartRedux.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  quantity: 0,
+  totoal: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.quantity = +1;
+      state.products.push(action.payload.products);
+      state.totoal = +action.payload.price;
+    },
+  },
+});
+
+export const { addProduct } = cartSlice.actions;
+export default cartSlice.reducer;
