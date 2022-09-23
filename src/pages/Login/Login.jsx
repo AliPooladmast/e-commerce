517fe0266b53dfe93593baf07e71f0feeb464ef3
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-23 15:46:31 +0330

    add username and password states and onChange functions

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./login.module.scss";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={style.Container}>
       <div className={style.Wrapper}>
         <h1>SIGN IN</h1>
         <form action="">
-          <input type="text" placeholder="username" />
-          <input type="text" placeholder="password" />
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button>LOGIN</button>
           <a href="">Forgot your password?</a>
           <a href="">Create an account</a>
