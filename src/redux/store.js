d7cac2b07e3e04f311fedac4fb2cb334851c4a38
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-23 14:42:44 +0330

    add user slice to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
+import userSlice from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
+    user: userSlice,
   },
 });
