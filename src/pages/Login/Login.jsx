674fe7dca1a6ba48a1851fa801e4a64143da7ea7
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-24 08:40:42 +0330

    set disabled mode for login button when fetching user data

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../redux/apiCalls";
 import style from "./login.module.scss";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const { error, isFetching } = useSelector((state) => state.user);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -28,7 +29,9 @@ const Login = () => {
             placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button onClick={handleClick}>LOGIN</button>
+          <button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </button>
           <a href="">Forgot your password?</a>
           <a href="">Create an account</a>
         </form>
