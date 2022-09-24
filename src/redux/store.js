e8d3074907dc43d28cbc623d050aedba96222375
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-24 22:18:20 +0330

    initialize the configuration of redux persist for user reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartSlice from "./cartSlice";
 import userSlice from "./userSlice";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userSlice);
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
-    user: userSlice,
+    user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
