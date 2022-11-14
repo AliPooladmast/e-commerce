29eb42b8624ceb50b91b44e7079948e386b8769d
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-14 16:56:18 +0330

    remove shop now button from categories

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -9,7 +9,6 @@ const CategoryItem = ({ item }) => {
         <img src={item.img} alt={item.title} />
         <div className={style.Info}>
           <h1>{item.title}</h1>
-          <button>SHOP NOW</button>
         </div>
       </Link>
     </div>
