50b643b08784a5280ed8be8afef5b03561f72863
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-14 12:17:32 +0330

    make login api call without token

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,11 +1,11 @@
-import { userRequest } from "../requestMethods";
+import { publicRequest } from "../requestMethods";
 import { loginFailure, loginStart, loginSuccess } from "./userSlice";
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await userRequest.post("/auth/login", user);
-    dispatch(loginSuccess(res.data));
+    const res = await publicRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res?.data));
   } catch (err) {
     dispatch(loginFailure());
   }
