874b2184a8e8cee1748e7859cfb824882df69389
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-14 14:37:57 +0330

    remove extra wrapper in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,36 +10,34 @@ const NavBar = () => {
 
   return (
     <div className={style.Container}>
-      <div className={style.Wrapper}>
-        <div className={style.Left}>
-          <Link to="/" className={style.Link}>
-            <h1 className={style.Logo}>E-Shop Client</h1>
-          </Link>
-          <div className={style.SearchContainer}>
-            <input className={style.Input} placeholder="Search"></input>
-            <Search className={style.Search} />
+      <div className={style.Left}>
+        <Link to="/" className={style.Link}>
+          <h1 className={style.Logo}>E-Shop Client</h1>
+        </Link>
+        <div className={style.SearchContainer}>
+          <input className={style.Input} placeholder="Search"></input>
+          <Search className={style.Search} />
+        </div>
+      </div>
+      <div className={style.Right}>
+        <Link to="/register" className={style.Link}>
+          <div>REGISTER</div>
+        </Link>
+        <Link to="login" className={style.Link}>
+          <div>SIGN IN</div>
+        </Link>
+        <Link to="/cart">
+          <div>
+            <Badge
+              badgeContent={quantity}
+              color="primary"
+              overlap="rectangular"
+              className={style.Badge}
+            >
+              <ShoppingCartOutlined />
+            </Badge>
           </div>
-        </div>
-        <div className={style.Right}>
-          <Link to="/register" className={style.Link}>
-            <div>REGISTER</div>
-          </Link>
-          <Link to="login" className={style.Link}>
-            <div>SIGN IN</div>
-          </Link>
-          <Link to="/cart">
-            <div>
-              <Badge
-                badgeContent={quantity}
-                color="primary"
-                overlap="rectangular"
-                className={style.Badge}
-              >
-                <ShoppingCartOutlined />
-              </Badge>
-            </div>
-          </Link>
-        </div>
+        </Link>
       </div>
     </div>
   );
