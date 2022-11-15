36288e28f3e73a17c3f463bb250ca3fa08cac428
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-15 16:06:37 +0330

    add register to api calls

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,10 +1,22 @@
 import { publicRequest, userRequest } from "../requestMethods";
 import { loginSuccess, userFailure, userStart } from "./userSlice";
 
+//User API Calls
 export const login = async (dispatch, user) => {
   dispatch(userStart());
   try {
     const res = await publicRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res?.data));
+    userRequest.defaults.headers.token = "Bearer " + res?.data?.token;
+  } catch (err) {
+    dispatch(userFailure());
+  }
+};
+
+export const register = async (dispatch, user) => {
+  dispatch(userStart());
+  try {
+    const res = await publicRequest.post("/auth/register", user);
     dispatch(loginSuccess(res?.data));
     userRequest.defaults.headers.token = "Bearer " + res?.data?.token;
   } catch (err) {
