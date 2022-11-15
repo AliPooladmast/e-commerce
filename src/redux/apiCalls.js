7cd9e43d4553583168e61b13ba279e630d5b846e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-15 13:43:03 +0330

    add token to local storage when login

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,4 +1,4 @@
-import { publicRequest } from "../requestMethods";
+import { publicRequest, userRequest } from "../requestMethods";
 import { loginSuccess, userFailure, userStart } from "./userSlice";
 
 export const login = async (dispatch, user) => {
@@ -6,6 +6,7 @@ export const login = async (dispatch, user) => {
   try {
     const res = await publicRequest.post("/auth/login", user);
     dispatch(loginSuccess(res?.data));
+    userRequest.defaults.headers.token = "Bearer " + res?.data?.token;
   } catch (err) {
     dispatch(userFailure());
   }
