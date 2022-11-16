9e82726db815dae9cb7b63ce7dc4cc7642301a86
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-16 23:12:11 +0330

    navigate to home when logout in user page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -3,26 +3,37 @@ import style from "./user.module.scss";
 import UserInfo from "../../components/userInfo/UserInfo";
 import EditUser from "../../components/editUser/EditUser";
 import NavBar from "../../components/NavBar/NavBar";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const User = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    !currentUser && navigate("/");
+  }, [currentUser]);
 
   return (
     <>
-      <NavBar />
-      <div className={style.UserComponent}>
-        <div className={style.TitleContainer}>
-          <h1>Edit User</h1>
-        </div>
-        <div className={style.UserContainer}>
-          <div className={style.UserInfo}>
-            <UserInfo user={currentUser} />
+      {currentUser && (
+        <>
+          <NavBar />
+          <div className={style.UserComponent}>
+            <div className={style.TitleContainer}>
+              <h1>Edit User</h1>
+            </div>
+            <div className={style.UserContainer}>
+              <div className={style.UserInfo}>
+                <UserInfo user={currentUser} />
+              </div>
+              <div className={style.EditUser}>
+                <EditUser user={currentUser} />
+              </div>
+            </div>
           </div>
-          <div className={style.EditUser}>
-            <EditUser user={currentUser} />
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
