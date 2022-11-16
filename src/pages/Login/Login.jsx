909d3c60ab1293c269e9d3acc45c85f7d38253ab
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-16 09:35:03 +0330

    add snack bar for joi errors

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,27 @@
+import { Alert, Snackbar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { login } from "../../redux/apiCalls";
 import style from "./login.module.scss";
 const Joi = require("joi");
 
 const schema = Joi.object({
   username: Joi.string().min(2).max(50).required(),
   password: Joi.string().min(5).max(1024).required(),
-  confirmPassword: Joi.ref("password"),
 });
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { error, isFetching, currentUser } = useSelector((state) => state.user);
+  const {
+    error: serverError,
+    isFetching,
+    currentUser,
+  } = useSelector((state) => state.user);
   const [input, setInput] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [showSnackbar, setShowSnackbar] = useState(false);
 
   const handleInput = (e) => {
     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -22,17 +29,41 @@ const Login = () => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(input);
+
+    const { error: joiError } = schema.validate(input);
+    if (joiError) {
+      setErrorMessage(joiError.details?.[0]?.message);
+      setShowSnackbar(true);
+    } else {
+      login(dispatch, input);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setShowSnackbar(false);
   };
 
   useEffect(() => {
-    if (currentUser && !error) {
+    if (currentUser && !serverError) {
       navigate("/");
     }
   }, [currentUser]); //eslint-disable-line
 
   return (
     <div className={style.Container}>
+      <Snackbar
+        open={showSnackbar}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          {errorMessage}
+        </Alert>
+      </Snackbar>
       <div className={style.Wrapper}>
         <h1>SIGN IN</h1>
         <form action="">
@@ -51,7 +82,6 @@ const Login = () => {
           <button onClick={handleClick} disabled={isFetching}>
             LOGIN
           </button>
-          {error && <div className={style.Error}>Somthing went wrong...</div>}
           <a href="register">Create an account</a>
         </form>
       </div>
