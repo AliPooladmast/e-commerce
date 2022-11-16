87879ca95e120e24c28d04f3b2be282e92ccf7b3
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-16 22:38:53 +0330

    add user page to router

---
+++ src/pages/User/User.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import style from "./user.module.scss";
+import UserInfo from "../../components/userInfo/UserInfo";
+import EditUser from "../../components/editUser/EditUser";
+import NavBar from "../../components/NavBar/NavBar";
+
+const User = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <>
+      <NavBar />
+      <div className={style.UserComponent}>
+        <div className={style.TitleContainer}>
+          <h1>Edit User</h1>
+        </div>
+        <div className={style.UserContainer}>
+          <div className={style.UserInfo}>
+            <UserInfo user={currentUser} />
+          </div>
+          <div className={style.EditUser}>
+            <EditUser user={currentUser} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default User;
