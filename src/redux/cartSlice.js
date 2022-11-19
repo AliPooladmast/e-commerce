fc9fdf93c1927532477ee467591097ff509ab5e2
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-19 11:01:57 +0330

    delete product to cart slice

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -15,8 +15,16 @@ const cartSlice = createSlice({
       state.products.push(action.payload);
       state.total += action.payload.price * action.payload.quantity;
     },
+    deleteProduct: (state, action) => {
+      state.quantity -= 1;
+      state.products.splice(
+        state.products.findIndex((item) => item._id === action.payload._id),
+        1
+      );
+      state.total -= action.payload.price * action.payload.quantity;
+    },
   },
 });
 
-export const { addProduct } = cartSlice.actions;
+export const { addProduct, deleteProduct } = cartSlice.actions;
 export default cartSlice.reducer;
