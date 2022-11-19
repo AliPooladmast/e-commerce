b05e29439197582335a8c577d54673e73f1254ee
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-19 13:20:57 +0330

    put condition for invite section related to current user

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Categories from "../../components/Categories/Categories";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
-import Newsletter from "../../components/Newsletter/Newsletter";
+import Invite from "../../components/Invite/Invite";
 import Products from "../../components/Products/Products";
 import Slider from "../../components/Slider/Slider";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div style={{ backgroundColor: "#f7fafa" }}>
       <NavBar />
       <Slider />
       <Categories />
       <Products />
-      <Newsletter />
+      {!currentUser && <Invite />}
       <Footer />
     </div>
   );
