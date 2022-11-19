a1114b667968912d561526d000a7b332a53cd5ff
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-19 13:18:08 +0330

    change name of invite module

--- src/components/Invite/Invite.jsx
+++ src/components/Invite/Invite.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Newsletter.module.scss";
+import style from "./Invite.module.scss";
 import { Link } from "react-router-dom";
 
 const Newsletter = () => {
