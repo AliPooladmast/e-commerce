004ff1c8c6f24f15a8303de1e79436c0bb7052cb
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-19 13:28:06 +0330

    add footer to user page

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -5,6 +5,7 @@ import EditUser from "../../components/editUser/EditUser";
 import NavBar from "../../components/NavBar/NavBar";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import Footer from "../../components/Footer/Footer";
 
 const User = () => {
   const { currentUser } = useSelector((state) => state.user);
@@ -32,6 +33,7 @@ const User = () => {
               </div>
             </div>
           </div>
+          <Footer />
         </>
       )}
     </>
