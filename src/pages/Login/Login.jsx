0939e58749e4ca309460f8de8ff02e400386c834
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-06 13:30:17 +0330

    update login page according to admin panel

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,8 @@
-import { Alert, Snackbar } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/apiCalls";
+import { setMessage } from "../../redux/uxSlice";
 import style from "./login.module.scss";
 const Joi = require("joi");
 
@@ -14,14 +14,8 @@ const schema = Joi.object({
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const {
-    error: serverError,
-    isFetching,
-    currentUser,
-  } = useSelector((state) => state.user);
+  const { currentUser, isFetching } = useSelector((state) => state.user);
   const [input, setInput] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [showSnackbar, setShowSnackbar] = useState(false);
 
   const handleInput = (e) => {
     setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
@@ -32,60 +26,48 @@ const Login = () => {
 
     const { error: joiError } = schema.validate(input);
     if (joiError) {
-      setErrorMessage(joiError.details?.[0]?.message);
-      setShowSnackbar(true);
+      dispatch(
+        setMessage({
+          type: "error",
+          text: joiError.details?.[0]?.message?.toString(),
+        })
+      );
     } else {
       login(dispatch, input);
     }
   };
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setShowSnackbar(false);
-  };
-
   useEffect(() => {
-    if (serverError) {
-      setErrorMessage(serverError);
-      setShowSnackbar(true);
-    } else if (currentUser) {
+    if (currentUser) {
       navigate("/");
     }
-  }, [currentUser, serverError]); //eslint-disable-line
+  }, [currentUser]); //eslint-disable-line
 
   return (
     <div className={style.Container}>
-      <Snackbar
-        open={showSnackbar}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-          {errorMessage}
-        </Alert>
-      </Snackbar>
       <div className={style.Wrapper}>
         <h1>SIGN IN</h1>
         <form action="">
-          <input
-            type="text"
-            placeholder="username"
-            name="username"
-            onChange={handleInput}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            onChange={handleInput}
-          />
-          <button onClick={handleClick} disabled={isFetching}>
-            LOGIN
-          </button>
-          <a href="register">Create an account</a>
+          <div className={style.InputContainer}>
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              onChange={handleInput}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              onChange={handleInput}
+            />
+            <a href="register">Create an account</a>
+          </div>
+          <div className={style.LoginButton}>
+            <button onClick={handleClick} disabled={isFetching}>
+              LOGIN
+            </button>
+          </div>
         </form>
       </div>
     </div>
