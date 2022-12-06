ecf645f54a3812942719c34bdb3f2847164348a2
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-06 10:34:07 +0330

    add ux slice to reducers and to persist blacklist

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,14 +12,20 @@ import {
 import storage from "redux-persist/lib/storage";
 import cartSlice from "./cartSlice";
 import userSlice from "./userSlice";
+import uxSlice from "./uxSlice";
 
 const persistConfig = {
   key: "root",
   version: 1,
   storage,
+  blacklist: ["ux"],
 };
 
-const rootReducer = combineReducers({ user: userSlice, cart: cartSlice });
+const rootReducer = combineReducers({
+  user: userSlice,
+  cart: cartSlice,
+  ux: uxSlice,
+});
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
