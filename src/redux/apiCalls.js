b1cd43bbb48785954bebbc6cedcad622cad1e902
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-11 14:24:50 +0330

    create api calls for orders based on admin panel

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,4 +1,12 @@
 import { publicRequest, userRequest } from "../requestMethods";
+import {
+  orderStart,
+  addOrderSuccess,
+  deleteOrderSuccess,
+  editOrderSuccess,
+  getOrderSuccess,
+  orderFailure,
+} from "./orderSlice";
 import { loginSuccess, userFailure, userStart } from "./userSlice";
 import { setMessage } from "./uxSlice";
 
@@ -65,6 +73,88 @@ export const editUser = async (dispatch, userId, user) => {
     }
   } catch (err) {
     dispatch(userFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+//Order API Calls
+export const getOrders = async (dispatch) => {
+  dispatch(orderStart());
+  try {
+    const res = await userRequest.get("/orders");
+    dispatch(getOrderSuccess(res?.data));
+  } catch (err) {
+    dispatch(orderFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const deleteOrder = async (dispatch, id) => {
+  dispatch(orderStart());
+  try {
+    const res = await userRequest.delete(`/orders/${id}`);
+
+    if (res) {
+      dispatch(deleteOrderSuccess(id));
+      dispatch(
+        setMessage({
+          type: "success",
+          text: "order has been deleted successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(orderFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const addOrder = async (dispatch, order) => {
+  dispatch(orderStart());
+  try {
+    const res = await userRequest.post("/orders", order);
+
+    if (res?.data) {
+      dispatch(addOrderSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text:
+            res.data.title?.toString() + " order has been created successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(orderFailure());
+    dispatch(
+      setMessage({ type: "error", text: err?.response?.data?.toString() })
+    );
+  }
+};
+
+export const editOrder = async (dispatch, orderId, order) => {
+  dispatch(orderStart());
+  try {
+    const res = await userRequest.put("/orders/" + orderId, order);
+
+    if (res?.data) {
+      dispatch(editOrderSuccess(res.data));
+      dispatch(
+        setMessage({
+          type: "success",
+          text:
+            res.data.title?.toString() + " order has been edited successfully",
+        })
+      );
+    }
+  } catch (err) {
+    dispatch(orderFailure());
     dispatch(
       setMessage({ type: "error", text: err?.response?.data?.toString() })
     );
