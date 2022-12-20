f433423355d8bd350e3fe488b1088d89d56df5fe
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-20 16:19:16 +0330

    fix data images directories

--- src/data.js
+++ src/data.js
@@ -1,33 +1,29 @@
-import fashionSet from "./assets/images/fashion-set.jpg";
-import girlSunDress from "./assets/images/girl-sundress.jpg";
-import menCoat from "./assets/images/men-coat.jpg";
-import girlSunGlasses from "./assets/images/girl-sunglass.jpg";
-import casualDress from "./assets/images/casual-dress.jpg";
-import fashionMen from "./assets/images/fashion-men.jpg";
-import formalDress from "./assets/images/formal-dress.jpg";
-
 export const sliderItems = [
   {
     id: 1,
-    img: fashionSet,
+    img: "./assets/images/fashion-set.jpg",
+    imgMobile: "./assets/images/fashion-set-mobile.jpg",
     title: "SEASONAL SALE",
     desc: "NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%",
   },
   {
     id: 2,
-    img: girlSunDress,
+    img: "./assets/images/girl-sundress.jpg",
+    imgMobile: "./assets/images/girl-sundress-mobile.jpg",
     title: "SPECTIAL COLLECTION",
     desc: "NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%",
   },
   {
     id: 3,
-    img: menCoat,
+    img: "./assets/images/men-coat.jpg",
+    imgMobile: "./assets/images/men-coat-mobile.jpg",
     title: "GIFT FOR FANS",
     desc: "NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%",
   },
   {
     id: 4,
-    img: girlSunGlasses,
+    img: "./assets/images/girl-sunglass.jpg",
+    imgMobile: "./assets/images/girl-sunglass-mobile.jpg",
     title: "FASHION PORDUCTS",
     desc: "NEW PRODUCTS WOULD BE DISCOUNTED UP TO 30%",
   },
@@ -36,19 +32,22 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: casualDress,
+    img: "./assets/images/casual-dress.jpg",
+    imgMobile: "./assets/images/casual-dress-mobile.jpg",
     title: "Casual Outfit",
     category: "casual",
   },
   {
     id: 2,
-    img: fashionMen,
+    img: "./assets/images/fashion-men.jpg",
+    imgMobile: "./assets/images/fashion-men-mobile.jpg",
     title: "Fashion Style",
     category: "fashion",
   },
   {
     id: 3,
-    img: formalDress,
+    img: "./assets/images/formal-dress.jpg",
+    imgMobile: "./assets/images/formal-dress-mobile.jpg",
     title: "Formal Dress",
     category: "formal",
   },
