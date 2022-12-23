d23da51c4836091b6d812898d281ce47dca2e4cd
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-23 22:12:57 +0330

    decrease number of env by summarizing fire base info

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
+  authDomain: `${process.env.REACT_APP_FIRE_BASE_PROJECT_ID}.firebaseapp.com`,
   projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
+  storageBucket: `${process.env.REACT_APP_FIRE_BASE_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
 };
