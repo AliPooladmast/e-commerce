75d84fdf1507570deb6b1c6620165f3f6ca9b940
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-02-10 23:02:59 +0330

    check the title of the catogry

---
+++ src/components/CategoryItem/__test__/CategoryItem.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import CategoryItem from "../CategoryItem";
+import renderer from "react-test-renderer";
+import { CategoryRounded } from "@mui/icons-material";
+
+const item = {
+  img: "img",
+  imgMobile: "imgMobile",
+  title: "title",
+  category: "category",
+};
+
+it("check if the title is correct", () => {
+  render(
+    <BrowserRouter>
+      <CategoryItem item={item} />
+    </BrowserRouter>
+  );
+
+  expect(
+    screen.getByTestId("categorty-link").querySelector("h1").textContent
+  ).toMatch("title");
+});
