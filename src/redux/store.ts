2b03e4e7292cc37cfa64915a0fd758f818317b17
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2023-02-22 23:27:26 +0330

    convert redux store to TS

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -42,3 +42,6 @@ export const store = configureStore({
 });
 
 export let persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
